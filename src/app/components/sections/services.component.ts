82e2acef52277025434adab71c32b7d6e1f23ffb
Derik <derikzgm@gmail.com>
2026-08-24 14:10:14 -0400

    prueba de push

--- src/app/components/sections/services.component.ts
+++ src/app/components/sections/services.component.ts
@@ -20,17 +20,39 @@ import { matAdd } from '@ng-icons/material-icons/baseline';
         <!-- Seccion de dulces a encargar: -->
         <div class="w-full">
           <!-- Grid de dulces -->
-          <div class="w-full text-xl">
+          <div class="w-full text-xl grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2
+          gap-4 sm:grid-cols-2">
             @for (encargo of encargos(); track $index) {
-              <div class="grid grid-cols-1 md:grid-cols-2 gap-4 overflow-hidden">
+              <!-- <div class="grid grid-cols-1 md:grid-cols-2 gap-4 
+              overflow-hidden mt-6"> -->
+
+              <div class="flex flex-col
+              overflow-hidden mt-6">
+              
+              <!-- Imagen previa -->
+              <div class="w-full bg-gray-100 flex items-center justify-center 
+                border-2 rounded-t border-gray-500 aspect-square">
+                @if (encargo.dulce.imagen) {
+                  <img 
+                    [src]="encargo.dulce.imagen" 
+                    [alt]="encargo.dulce.nombre"
+                    class="w-full h-full object-cover aspect-square">
+                } @else {
+                  <p class="text-gray-400 text-sm select-none aspect-square 
+                  items-center flex">Vista previa</p>
+                }
+              </div>
+
                 <!-- Formulario del encargo - Ocupa exactamente la mitad -->
-                <div class="p-2 space-y-4 bg-white border-2 rounded border-gray-500">
+                <div class="flex flex-col p-2 bg-white h-full border-b-2
+                  border-l-2 border-r-2 rounded-b border-gray-500 justify-around space-y-4">
+                  <!-- nombre -->
                   <div class="flex w-full justify-between items-center">
                     <p class="font-medium select-none">Dulce</p>
                     <select 
                       [value]="encargo.dulce.nombre"
                       (change)="manejar_cambio_de_dulce($index, $event)"
-                      class="rounded px-2 py-1 w-1/2 outline outline-2">
+                      class="rounded px-2 py-1 w-1/2 outline-2">
                       @for (dulce of tipos_de_dulces; track $index) {
                         <option [value]="dulce.nombre">{{dulce.nombre}}</option>
                       }
@@ -44,38 +66,26 @@ import { matAdd } from '@ng-icons/material-icons/baseline';
                       type="number" 
                       min="1" 
                       [value]="encargo.cantidad"
-                      class="outline outline-2 rounded w-1/2 text-center px-2 py-1"
+                      class="outline-2 rounded w-1/2 text-center px-2 py-1"
                       (input)="manejar_cambio_de_cantidad($index, $event.target)">
                   </div>
-                </div>
-
-                <!-- Imagen previa - Ocupa exactamente la otra mitad -->
-                <div class="w-full bg-gray-100 flex items-center justify-center 
-                min-h-[150px] md:min-h-0 border-2 rounded border-gray-500">
-                  @if (encargo.dulce.imagen) {
-                    <img 
-                      [src]="encargo.dulce.imagen" 
-                      [alt]="encargo.dulce.nombre"
-                      class="w-full h-full object-cover">
-                  } @else {
-                    <p class="text-gray-400 text-sm select-none">Vista previa</p>
-                  }
                 </div>
               </div>
             }
           </div>
 
           <!-- Botón agregar -->
-          <div class="w-full flex justify-between my-4 items-center 
-          cursor-pointer agregar-padre transition-colors">
-            <div class="w-full flex justify-center items-center">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-4 
+          justify-between my-4 items-center transition-colors"
+          (click)="agregar_encargo()">
+            <div class="w-full flex justify-center items-center agregar-padre cursor-pointer">
               <div class="flex justify-center items-center border-2 rounded">
                 <ng-icon class="text-4xl border-r-2" name="agregar" />
                 <span class="text-2xl px-2 select-none">Agregar otro dulce</span>
               </div>
             </div>
 
-            <div class="flex w-full justify-center space-x-2 text-2xl">
+            <div class="flex w-full justify-center space-x-2 text-2xl select-none">
               <p>Precio Estimado:</p>
               <p>{{precio()}} CUP</p>
             </div>
@@ -89,14 +99,12 @@ import { matAdd } from '@ng-icons/material-icons/baseline';
         @reference "tailwindcss";
         div {
           @apply border-green-700;
-          div{
-            ng-icon {
-              @apply text-green-700;
-            }
 
-            span{
-              @apply text-green-700;
-            }
+          ng-icon {
+            @apply text-green-700;
+          }
+          span{
+            @apply text-green-700;
           }
         }
       }
@@ -104,7 +112,7 @@ import { matAdd } from '@ng-icons/material-icons/baseline';
   `,
   imports: [NgIcon]
 })
-export class ServicesComponent implements OnInit{
+export class ServicesComponent implements OnInit {
 
   precio = computed<number>(() => {
     let precio = 0;
@@ -118,10 +126,7 @@ export class ServicesComponent implements OnInit{
   encargos_no_signal: Encargo [] = []
 
   ngOnInit(): void {
-    this.tipos_de_dulces.push({
-      precio: 2000,
-      nombre: 'Cake Comun'
-    })
+    this.tipos_de_dulces.push(...tipos_de_dulces);
 
     this.encargos.set([{
       dulce: this.tipos_de_dulces[0],
@@ -156,7 +161,7 @@ manejar_cambio_de_dulce(encargo_index: number, event: Event) {
         ...encargos,
         {
           dulce: this.tipos_de_dulces[0], // Por defecto el primer dulce
-          cantidad: 1
+          cantidad: 1,
         }
       ];
     });
@@ -191,3 +196,19 @@ export interface Encargo {
   dulce: Dulce,
   cantidad: number,
 }
+
+//Tipos de dulces:
+let tipos_de_dulces: Dulce[] = [
+  {
+    precio: 4500,
+    nombre: 'Chocoflan con merenge'
+  },
+  {
+    precio: 2000,
+    nombre: 'Flan mediano (15 cm)'
+  },
+  {
+    precio: 3000,
+    nombre: 'Flan Grande (18 cm)'
+  },
+]
